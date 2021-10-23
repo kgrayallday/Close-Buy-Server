550e7bafaa72112667a8203d58833e2834ee4e56
Rdarke <rickydarke1@gmail.com>
2021-10-23 15:34:55 -0700

    Adjust products table schema

--- back-end/db/schema/02_products.sql
+++ back-end/db/schema/02_products.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  primary_url VARCHAR(255) NOT NULL,
+  primary_url TEXT NOT NULL,
   domain VARCHAR(255) NOT NULL,
   price INTEGER NOT NULL,
 );
