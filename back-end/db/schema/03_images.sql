8e82926686e1845dec155847b38065e470aabdde
Rdarke <rickydarke1@gmail.com>
2021-10-23 15:10:51 -0700

    Create images table

---
+++ back-end/db/schema/03_images.sql
@@ -0,0 +1,8 @@
+-- Drop and recreate images table
+
+DROP TABLE IF EXISTS images CASCADE;
+CREATE TABLE images (
+  id SERIAL PRIMARY KEY NOT NULL,
+  product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+  url TEXT NOT NULL,
+);
