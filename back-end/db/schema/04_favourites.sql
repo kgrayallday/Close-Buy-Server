f4ed8b818f9636f5f130bdd2d2b5028f11d20787
Rdarke <rickydarke1@gmail.com>
2021-10-23 15:13:21 -0700

    Create favourites Db table

---
+++ back-end/db/schema/04_favourites.sql
@@ -0,0 +1,8 @@
+-- Drop and recreate favourites table
+
+DROP TABLE IF EXISTS favourites CASCADE;
+CREATE TABLE favourites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+);
