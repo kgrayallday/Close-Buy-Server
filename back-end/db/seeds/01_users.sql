fd7350f5b45f7d42d9ce3a2d1af3eb929750cf8b
Rdarke <rickydarke1@gmail.com>
2021-10-22 21:00:09 -0700

    Create user seeds

---
+++ back-end/db/seeds/01_users.sql
@@ -0,0 +1,4 @@
+-- Users table seeds here
+
+INSERT INTO users (name, email, password)
+VALUES ('Ricky Bobby', 'iamfake@gmail.com', 'password'), ('Kyle Gray', 'test@testing.com', 'password'), ('Sanghwan', 'tesrt@gmail.com', 'password');
