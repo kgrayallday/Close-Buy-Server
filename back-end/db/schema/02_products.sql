310085c948d558d72682c76be1d4a1dd861e7449
Rdarke <rickydarke1@gmail.com>
2021-10-23 15:07:28 -0700

    Create products DB table

---
+++ back-end/db/schema/02_products.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate Products table --
+
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  primary_url VARCHAR(255) NOT NULL,
+  domain VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+);
