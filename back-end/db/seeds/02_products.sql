751f6f33a5109751c8478a4deaf4c78bd7e27bff
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-10-27 11:54:52 -0700

    Add seeds files

---
+++ back-end/db/seeds/02_products.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO products (name, primary_url, domain, price)
+-- VALUES 
+--   ('Pair of rattan / wicker arm chairs - $198 (Richmond)', 'https://vancouver.craigslist.org/rch/fuo/d/richmond-pair-of-rattan-wicker-arm/7389634388.html', 'TBD_craigslist_7389634388', 198), 
+--   ('Nice Elite Sectional Comfy sofa couch - Free Delivery - $700 (Vancouver)', 'https://vancouver.craigslist.org/van/fuo/d/vancouver-nice-elite-sectional-comfy/7399538340.html', 'TBD_craigslist_7399538340', 700), 
+--   ('Blue Corduroy Sectional Couch - FREE DELIVERY - $700', 'https://vancouver.craigslist.org/van/fuo/d/burnaby-blue-corduroy-sectional-couch/7397460944.html', 'TBD_craigslist_7397460944', 700);
