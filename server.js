bf96e8fc51234d89f709b31e6233b5e12bbd61b4
Kyle Gray <kylemaxgray@gmail.com>
2022-02-10 11:12:46 -0800

    fix case of variables

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 // Web server config
 const PORT = process.env.PORT || 8080;
 
-const Express = require('express');
-const App = Express();
-const BodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const db = require("./lib/queries");
 
-// Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
+// express Configuration
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 // Separated Routes for each Resource 
 // Example -- const products = require("./routes/products");
@@ -23,16 +23,16 @@ const favouritesRoutes = require("./routes/favourites");
 // Mount all resource routes
 // Example -- app.use('/api/products', productRoutes);
 // Example -- app.use('/api/users', userRoutes);
-App.use('/api/products', productRoutes(db));
-App.use('/api/users', userRoutes(db));
-App.use('/api/favourites', favouritesRoutes(db));
+app.use('/api/products', productRoutes(db));
+app.use('/api/users', userRoutes(db));
+app.use('/api/favourites', favouritesRoutes(db));
 
 // Home GET route
-App.get('/api/data', (req, res) => res.json({
+app.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
 
-App.listen(PORT, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(`express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
